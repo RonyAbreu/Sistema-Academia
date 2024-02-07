74cde3f7aacc5625f882598b76e3de991eea2de3
RonyAbreu <ronyelison78@gmail.com>
2024-02-06 21:41:27 -0300

    configurando cors da aplicação

--- backend/src/main/java/com/ronyelison/docker/config/WebConfig.java
+++ backend/src/main/java/com/ronyelison/docker/config/WebConfig.java
@@ -7,12 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${cors.origins}")
-    private String origins;
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings( CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(origins)
+                .allowedOrigins("http://127.0.0.1:5501/")
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowCredentials(true);
